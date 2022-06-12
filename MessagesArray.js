672d90e1f61b8d99cf5a3200d22e3d3f7e6016ae
Guiwv <gwvasconcelos.dev@gmail.com>
2022-06-12 17:35:00 -0300

    add MessagesArray

---
+++ MessagesArray.js
@@ -0,0 +1,6 @@
+//This file contains the array of the messages used in the Mixed Messages Project
+const messages = ["Melhor começar trabalhando!", 
+"Vamos descansar antes de qualquer coisa", 
+"Que tal jogar um jogo?", 
+"Dia de fazer um café da manhã mais caprichado!", 
+"Dia para começar passeando com o cachorro!"]
